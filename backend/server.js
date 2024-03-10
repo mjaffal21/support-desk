f5374d94b1840ed542c5fc334f71c592a247cd9a
mjaffal21 <jeffjaffal@icloud.com>
2024-03-10 14:11:19 +0200

    final 4

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,20 @@ const ticketsRoute = require("./routes/ticketRoute");
 app.use("/api/users", usersRoute);
 app.use("/api/tickets", ticketsRoute);
 
+const __dir = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dir, "/frontend/build")));
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dir, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.use(asyncHandler);
 app.use(errorHandler);
 
